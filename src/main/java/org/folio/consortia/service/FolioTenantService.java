20ede165b3971ef8a2fd50f66a20312ef9eae016
Yaroslav_Kiriak <Yaroslav_Kiriak@epam.com>
2024-08-08 12:21:12 +0300

    EUREKA-65: add script to migrate data from mod-consortia

--- src/main/java/org/folio/consortia/service/FolioTenantService.java
+++ src/main/java/org/folio/consortia/service/FolioTenantService.java
@@ -1,6 +1,9 @@
 package org.folio.consortia.service;
 
 import java.sql.ResultSet;
+import java.util.Map;
+import liquibase.exception.LiquibaseException;
+import liquibase.exception.UnexpectedLiquibaseException;
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.lang3.ObjectUtils;
@@ -9,6 +12,7 @@ import org.folio.consortia.config.property.CustomFieldsRetryProperties;
 import org.folio.consortia.domain.dto.CustomField;
 import org.folio.consortia.domain.dto.CustomFieldType;
 import org.folio.spring.FolioExecutionContext;
+import org.folio.spring.exception.TenantUpgradeException;
 import org.folio.spring.liquibase.FolioSpringLiquibase;
 import org.folio.spring.service.PrepareSystemUserService;
 import org.folio.spring.service.SystemUserScopedExecutionService;
@@ -26,7 +30,8 @@ import org.springframework.stereotype.Service;
 @Primary
 public class FolioTenantService extends TenantService {
 
-  private static final String EXIST_SQL = "SELECT EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = ?)";
+  private static final String EXIST_SQL =
+    "SELECT EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = ?)";
 
   private final KafkaService kafkaService;
   private final CustomFieldService customFieldService;
@@ -36,6 +41,7 @@ public class FolioTenantService extends TenantService {
   private final CustomFieldsRetryProperties customFieldsRetryProperties;
 
   private static final String ORIGINAL_TENANT_ID_NAME = "originalTenantId";
+  private static final String TENANT_NAME_PARAMETER = "tenantname";
   private static final CustomField ORIGINAL_TENANT_ID_CUSTOM_FIELD = CustomField.builder()
     .name(ORIGINAL_TENANT_ID_NAME)
     .entityType("user")
@@ -62,6 +68,31 @@ public class FolioTenantService extends TenantService {
     this.customFieldsRetryProperties = customFieldsRetryProperties;
   }
 
+  /*
+   * Because of the liquibase.Scope implementation for the SpringLiquibase it is not possible to run several
+   * SpringLiquibase executions simultaneously. That is why this method must be synchronized.
+   */
+  @Override
+  public synchronized void createOrUpdateTenant(TenantAttributes tenantAttributes) {
+    if (folioSpringLiquibase != null) {
+      var params = Map.of(TENANT_NAME_PARAMETER, folioExecutionContext.getTenantId());
+      folioSpringLiquibase.setChangeLogParameters(params);
+      log.info("Set ChangeLog parameters: {}", params);
+
+      folioSpringLiquibase.setDefaultSchema(getSchemaName());
+      log.info("About to start liquibase update for tenant [{}]", context.getTenantId());
+
+      try {
+        folioSpringLiquibase.performLiquibaseUpdate();
+      } catch (LiquibaseException | UnexpectedLiquibaseException e) {
+        throw new TenantUpgradeException(e);
+      }
+      log.info("Liquibase update for tenant [{}] executed successfully", context.getTenantId());
+    }
+
+    afterTenantUpdate(tenantAttributes);
+  }
+
   @Override
   protected void afterTenantUpdate(TenantAttributes tenantAttributes) {
     try {
@@ -75,8 +106,8 @@ public class FolioTenantService extends TenantService {
   }
 
   /**
-   * Implemented by HSQLDB way
-   * Check if the tenant exists (by way of its database schema)
+   * Implemented by HSQLDB way. Check if the tenant exists (by way of its database schema)
+   *
    * @return if the tenant's database schema exists
    */
   @Override
@@ -92,7 +123,8 @@ public class FolioTenantService extends TenantService {
   private void createOriginalTenantIdCustomField() {
     systemUserScopedExecutionService.executeAsyncSystemUserScoped(folioExecutionContext.getTenantId(), () -> {
       if (ObjectUtils.isNotEmpty(customFieldService.getCustomFieldByName(ORIGINAL_TENANT_ID_NAME))) {
-        log.info("createOriginalTenantIdCustomField:: custom-field already available in tenant {} with name {}", folioExecutionContext.getTenantId(), ORIGINAL_TENANT_ID_NAME);
+        log.info("createOriginalTenantIdCustomField:: custom-field already available in tenant {} with name {}",
+          folioExecutionContext.getTenantId(), ORIGINAL_TENANT_ID_NAME);
       } else {
         createCustomFieldsWithRetry();
       }
