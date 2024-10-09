ac67c50b2cc1d46ee1818466b6945c51705e541b
Azizbek Khushvakov <azizbek_khushvakov@epam.com>
2024-10-09 18:29:45 +0500

    Fixed url name for feign client

--- src/main/java/org/folio/consortia/client/RoleCapabilitySetsClient.java
+++ src/main/java/org/folio/consortia/client/RoleCapabilitySetsClient.java
@@ -7,9 +7,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "role-capability-sets")
+@FeignClient(name = "role-capability-sets", url = "roles")
 public interface RoleCapabilitySetsClient {
 
-  @GetMapping(value = "/roles/{roleId}/capability-sets")
+  @GetMapping(value = "/{roleId}/capability-sets")
   CapabilitySets getRoleCapabilitySetsRoleId(@PathVariable UUID roleId);
 }
