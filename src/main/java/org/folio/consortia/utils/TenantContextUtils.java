25717294b5e6e2ad2188571badad3938e5001b00
Azizbek Khushvakov <azizbek_khushvakov@epam.com>
2025-01-20 18:38:02 +0500

    Convert CaseSensitiveMap to Map

--- src/main/java/org/folio/consortia/utils/TenantContextUtils.java
+++ src/main/java/org/folio/consortia/utils/TenantContextUtils.java
@@ -3,7 +3,6 @@ package org.folio.consortia.utils;
 import lombok.experimental.UtilityClass;
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.collections4.MapUtils;
-import org.apache.commons.lang3.SerializationUtils;
 import org.folio.spring.DefaultFolioExecutionContext;
 import org.folio.spring.FolioExecutionContext;
 import org.folio.spring.FolioModuleMetadata;
@@ -45,8 +44,10 @@ public class TenantContextUtils {
    */
   public static FolioExecutionContext prepareContextForTenant(String tenantId, FolioModuleMetadata folioModuleMetadata, FolioExecutionContext context) {
     if (MapUtils.isNotEmpty(context.getOkapiHeaders())) {
-      // create deep copy of headers in order to make switching context thread safe
-      var headersCopy = SerializationUtils.clone((HashMap<String, Collection<String>>) context.getAllHeaders());
+      Map<String, Collection<String>> headersCopy = new HashMap<>();
+      context.getAllHeaders().forEach((key, value) ->
+        headersCopy.put(key, List.copyOf(value))
+      );
       headersCopy.put(XOkapiHeaders.TENANT, List.of(tenantId));
       log.info("FOLIO context initialized with tenant {}", tenantId);
       return new DefaultFolioExecutionContext(folioModuleMetadata, headersCopy);
