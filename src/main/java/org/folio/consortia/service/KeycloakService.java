158d326f1726fbf02dced27d35cfaabe466b57c3
saba_zedginidze <saba_zedginidze@epam.com>
2025-02-17 23:36:44 +0400

    [MODCONSKC-63] Manage IDPs on tenant creation and deletion

--- src/main/java/org/folio/consortia/service/KeycloakService.java
+++ src/main/java/org/folio/consortia/service/KeycloakService.java
@@ -3,9 +3,10 @@ package org.folio.consortia.service;
 public interface KeycloakService {
 
   /**
-   * Creates an identity provider in the central tenant realm for the member tenant.
-   * In case the identity provider already exists, it will not be created again to
-   * support tenant removal and re-creation.
+   * Creates an identity provider in the central tenant realm for the member tenant with conditions:<br/>
+   * 1) In case the identity provider already exists, it will not be created again to
+   * support tenant removal and re-creation.<br/>
+   * 2) If the identity provider creation is disabled, the method will return without any action
    *
    * @param centralTenant central tenant
    * @param memberTenant  member tenant
@@ -13,7 +14,8 @@ public interface KeycloakService {
   void createIdentityProvider(String centralTenant, String memberTenant);
 
   /**
-   * Deletes an identity provider in the central tenant realm corresponding to the member tenant.
+   * Deletes an identity provider in the central tenant realm corresponding to the member tenant.<br/>
+   * If the identity provider creation is disabled, the method will return without any action
    *
    * @param centralTenant central tenant
    * @param memberTenant  member tenant
