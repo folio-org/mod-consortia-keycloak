6e0236ea96b6c2f6d3483dd8d2bd7bdf9fa9ba06
saba_zedginidze <saba_zedginidze@epam.com>
2025-02-18 19:10:25 +0400

    [MODCONSKC-63] Add KeycloakCredentialsService to manage client credentials

--- src/main/java/org/folio/consortia/client/KeycloakClient.java
+++ src/main/java/org/folio/consortia/client/KeycloakClient.java
@@ -1,26 +1,42 @@
 package org.folio.consortia.client;
 
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED_VALUE;
+
+import java.util.Map;
+
 import org.folio.consortia.config.keycloak.KeycloakFeignClientConfig;
 import org.folio.consortia.domain.dto.KeycloakIdentityProvider;
+import org.folio.consortia.domain.dto.KeycloakTokenResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "keycloak",
   url = "#{keycloakProperties.url}",
   configuration = KeycloakFeignClientConfig.class)
 public interface KeycloakClient {
 
+  @PostMapping(value = "/realms/master/protocol/openid-connect/token", consumes = APPLICATION_FORM_URLENCODED_VALUE)
+  KeycloakTokenResponse login(@RequestBody Map<String, ?> loginRequest);
+
   @GetMapping("/admin/realms/{realm}/identity-provider/instances/{alias}")
-  KeycloakIdentityProvider getIdentityProvider(@PathVariable("realm") String realm, @PathVariable("alias") String alias);
+  KeycloakIdentityProvider getIdentityProvider(@PathVariable("realm") String realm,
+                                               @PathVariable("alias") String alias,
+                                               @RequestHeader(AUTHORIZATION) String token);
 
   @DeleteMapping("/admin/realms/{realm}/identity-provider/instances/{alias}")
-  void deleteIdentityProvider(@PathVariable("realm") String realm, @PathVariable("alias") String alias);
+  void deleteIdentityProvider(@PathVariable("realm") String realm,
+                              @PathVariable("alias") String alias,
+                              @RequestHeader(AUTHORIZATION) String token);
 
   @PostMapping("/admin/realms/{realm}/identity-provider/instances")
-  void createIdentityProvider(@PathVariable("realm") String realm, @RequestBody KeycloakIdentityProvider identityProvider);
+  void createIdentityProvider(@PathVariable("realm") String realm,
+                              @RequestBody KeycloakIdentityProvider identityProvider,
+                              @RequestHeader(AUTHORIZATION) String token);
 
 }
---
+++ src/main/java/org/folio/consortia/service/impl/KeycloakCredentialsServiceImpl.java
@@ -0,0 +1,87 @@
+package org.folio.consortia.service.impl;
+
+import java.time.Instant;
+import java.util.HashMap;
+
+import org.folio.consortia.client.KeycloakClient;
+import org.folio.consortia.config.keycloak.KeycloakLoginClientProperties;
+import org.folio.consortia.config.keycloak.KeycloakProperties;
+import org.folio.consortia.domain.dto.KeycloakClientCredentials;
+import org.folio.consortia.service.KeycloakCredentialsService;
+import org.folio.tools.store.SecureStore;
+import org.folio.tools.store.exception.NotFoundException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Service
+@RequiredArgsConstructor
+@Log4j2
+public class KeycloakCredentialsServiceImpl implements KeycloakCredentialsService {
+
+  private static final String MASTER_REALM = "master";
+  private static final String MASTER_TOKEN_CACHE_KEY = "'keycloak-admin-cli-token'";
+
+  private final KeycloakClient keycloakClient;
+  private final KeycloakProperties keycloakProperties;
+  private final KeycloakLoginClientProperties keycloakClientProperties;
+  private final SecureStore secureStore;
+  private final TaskScheduler asyncTaskScheduler;
+
+  @Value("${folio.environment}")
+  private String folioEnvironment;
+
+  @Cacheable(cacheNames = "keycloak-credentials", key = "{#centralTenant, #memberTenant}")
+  public KeycloakClientCredentials getClientCredentials(String centralTenant, String memberTenant) {
+    var clientId = memberTenant + keycloakClientProperties.getClientNameSuffix();
+    var clientSecret = retrieveKcClientSecret(centralTenant, clientId);
+    return new KeycloakClientCredentials(clientId, clientSecret);
+  }
+
+  @Cacheable(cacheNames = "keycloak-token", key = MASTER_TOKEN_CACHE_KEY)
+  public String getMasterAuthToken() {
+    var clientId = keycloakProperties.getClientId();
+    var clientSecret = retrieveKcClientSecret(MASTER_REALM, clientId);
+
+    HashMap<String, String> loginRequest = new HashMap<>();
+    loginRequest.put("client_id", clientId);
+    loginRequest.put("client_secret", clientSecret);
+    loginRequest.put("grant_type", keycloakProperties.getGrantType());
+
+    log.info("requestToken:: Issuing access token for Keycloak communication [clientId: {}]", clientId);
+    var token = keycloakClient.login(loginRequest);
+
+    if (token.getExpiresIn() != null) {
+      // Evict token 60 seconds before expiration time
+      var expirationTime = Instant.now().plusSeconds(token.getExpiresIn()).minusSeconds(60);
+      asyncTaskScheduler.schedule(this::evictMasterAuthToken, expirationTime);
+    }
+
+    return token.getTokenType() + " " + token.getAccessToken();
+  }
+
+  @CacheEvict(cacheNames = "keycloak-token", key = MASTER_TOKEN_CACHE_KEY)
+  public void evictMasterAuthToken() {
+    log.info("evictMasterAuthToken:: Evicting master realm admin token from cache");
+  }
+
+  private String retrieveKcClientSecret(String realm, String clientId) {
+    if (Boolean.TRUE.equals(keycloakClientProperties.getSecureStoreDisabled())) {
+      log.info("retrieveKcClientSecret:: Secure store is disabled. Using default client secret");
+      return "SecretPassword";
+    }
+    try {
+      log.info("retrieveKcClientSecret:: Retrieving client secret from secure store");
+      return secureStore.get("%s_%s_%s".formatted(folioEnvironment, realm, clientId));
+    } catch (NotFoundException e) {
+      log.error("retrieveKcClientSecret:: Client secret not found in secure store [clientId: {}]", clientId);
+      throw new IllegalStateException("Failed to get value from secure store [clientId: %s]".formatted(clientId), e);
+    }
+  }
+
+}
