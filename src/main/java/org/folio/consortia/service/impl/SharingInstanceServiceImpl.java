5b1c3a2c1d74bdd093065e44931501eae56090bf
Serhii Nosko <v-snosko@ebsco.com>
2025-08-07 14:56:41 +0300

    MODCONSKC-82. The local instance is not shared (#176)
    * MODCONSKC-82. The local instance is not shared
    
    * MODCONSKC-82. Remove unused import

--- src/main/java/org/folio/consortia/service/impl/SharingInstanceServiceImpl.java
+++ src/main/java/org/folio/consortia/service/impl/SharingInstanceServiceImpl.java
@@ -68,6 +68,9 @@ public class SharingInstanceServiceImpl implements SharingInstanceService {
     String centralTenantId = tenantService.getCentralTenantId();
     String sourceTenantId = sharingInstance.getSourceTenantId();
     String targetTenantId = sharingInstance.getTargetTenantId();
+    if (Objects.isNull(sharingInstance.getId())) {
+      sharingInstance.setId(UUID.randomUUID());
+    }
     checkTenantsExistAndContainCentralTenantOrThrow(sourceTenantId, targetTenantId);
 
     if (Objects.equals(centralTenantId, sourceTenantId)) {
@@ -215,7 +218,7 @@ public class SharingInstanceServiceImpl implements SharingInstanceService {
 
   private SharingInstanceEntity toEntity(SharingInstance dto) {
     SharingInstanceEntity entity = new SharingInstanceEntity();
-    entity.setId(UUID.randomUUID());
+    entity.setId(dto.getId());
     entity.setInstanceId(dto.getInstanceIdentifier());
     entity.setSourceTenantId(dto.getSourceTenantId());
     entity.setTargetTenantId(dto.getTargetTenantId());
