a58e0aa59b6676ef18a07cc479ccd6fafbad0c1e
Mikita Siadykh <mikita_siadykh@epam.com>
2025-08-14 09:20:34 +0300

    test_url_endpoint

--- src/main/java/org/folio/consortia/client/UserRolesClient.java
+++ src/main/java/org/folio/consortia/client/UserRolesClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "roles-user", url = "/roles/users", configuration = FeignClientConfiguration.class)
+@FeignClient(name = "roles-user", url = "roles/users", configuration = FeignClientConfiguration.class)
 public interface UserRolesClient {
 
   @DeleteMapping("/{userId}")
