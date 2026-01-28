06e324c6d97a87e1a6f56703757d4f4f7ed39066
BKadirkhodjaev <b.kadirkhodjaev@gmail.com>
2026-01-28 18:57:00 +0500

    [MODCONSKC-100]. Remove post construct

--- src/main/java/org/folio/consortia/FolioConsortiaApplication.java
+++ src/main/java/org/folio/consortia/FolioConsortiaApplication.java
@@ -1,16 +1,11 @@
 package org.folio.consortia;
 
-import jakarta.annotation.PostConstruct;
-import lombok.extern.log4j.Log4j2;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.security.KeyStore;
-import java.security.KeyStoreException;
 import java.security.Security;
 
-@Log4j2
 @SpringBootApplication
 public class FolioConsortiaApplication {
 
@@ -22,11 +17,5 @@ public class FolioConsortiaApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(FolioConsortiaApplication.class, args);
-  }
-
-  @PostConstruct
-  public void init() throws KeyStoreException {
-    var instance = KeyStore.getInstance(BCFKS_KEYSTORE_TYPE);
-    log.info("Loaded security provider: {}", instance.getProvider().getName());
   }
 }
