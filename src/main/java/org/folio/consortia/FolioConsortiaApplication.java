e8a772286bb867125572aa8a11535881a96869f1
BKadirkhodjaev <b.kadirkhodjaev@gmail.com>
2026-01-28 18:04:58 +0500

    [MODCONSKC-100]. Custom keycloak login flow for central tenant failed on TLS environment

--- src/main/java/org/folio/consortia/FolioConsortiaApplication.java
+++ src/main/java/org/folio/consortia/FolioConsortiaApplication.java
@@ -1,12 +1,26 @@
 package org.folio.consortia;
 
+import jakarta.annotation.PostConstruct;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+
+import static org.folio.consortia.config.keycloak.KeycloakFeignClientConfig.BCFKS_KEYSTORE_TYPE;
+
+@Log4j2
 @SpringBootApplication
 public class FolioConsortiaApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(FolioConsortiaApplication.class, args);
+  }
+
+  @PostConstruct
+  public void init() throws KeyStoreException {
+    var instance = KeyStore.getInstance(BCFKS_KEYSTORE_TYPE);
+    log.info("Loaded BCFKS Provider: {}", instance.getProvider().getName());
   }
 }
---
+++ src/test/java/org/folio/consortia/config/keycloak/KeycloakFeignClientConfigTest.java
@@ -0,0 +1,19 @@
+package org.folio.consortia.config.keycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.security.KeyStore;
+
+import static org.folio.consortia.config.keycloak.KeycloakFeignClientConfig.BCFKS_KEYSTORE_TYPE;
+import static org.junit.jupiter.api.Assertions.*;
+
+// Load all classes to test the provider
+@SpringBootTest
+class KeycloakFeignClientConfigTest {
+
+  @Test
+  void verifyBouncyCastleIsRegistered() throws Exception {
+    assertNotNull(KeyStore.getInstance(BCFKS_KEYSTORE_TYPE));
+  }
+}
